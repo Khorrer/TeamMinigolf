83e0acc646d8f875b932c164cf10ca3ffd1390e3
Khorrer <j.karrer12@gmail.com>
2026-03-10 00:51:49 +0100

    BugFix

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+# Diese beiden neuen Zeilen hinzufügen
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
 ]
+
+# Dieser Block erlaubt Django, die CSS-Dateien während der Entwicklung auszuliefern
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
